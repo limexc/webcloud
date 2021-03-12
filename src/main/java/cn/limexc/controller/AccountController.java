65f6b4e87a1f7b0410a236888b679983a24f63e4
limexc <1375858836@qq.com>
2021-03-12 17:35:56 +0800

    邮件发送工具

--- src/main/java/cn/limexc/controller/AccountController.java
+++ src/main/java/cn/limexc/controller/AccountController.java
@@ -4,9 +4,9 @@ import cn.limexc.model.User;
 import cn.limexc.service.UserService;
 import cn.limexc.util.MailUtils;
 import cn.limexc.util.TimeUtils;
-import com.sun.org.apache.xpath.internal.operations.Mod;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.ModelAndView;
 
 import javax.annotation.Resource;
@@ -48,7 +48,8 @@ public class AccountController {
 
 
         System.out.println("userlogin   "+username+"  "+password+"  "+rs);
-        mv.setViewName("redirect:/index.jsp");
+        //mv.setViewName("redirect:/index.jsp");
+        mv.setViewName("forward:/index.jsp");
         return mv;
     }
 
@@ -67,21 +68,20 @@ public class AccountController {
      * @param req
      */
     @RequestMapping(value = "/sendmail")
+    @ResponseBody
     public void sendMail(HttpServletRequest req){
         String email = req.getParameter("email");
 
-        System.out.println("获取到用户输入的邮箱： "+email);
+        System.out.println("["+TimeUtils.timeUtils.getForMatTime()+"]"+"获取到用户输入的邮箱： "+email);
 
         StringBuffer text=new StringBuffer("<h1>来自CloudWeb的验证码</h1>");
         text.append("<h3>用户："+email+" 您好</h3>");
         text.append("本次的验证码为");
-
-
         text.append("<p>如果您没有在注册账号或更改您的账号信息，请忽略此电子邮件。</p></br>");
         text.append("<p>不要把链接给任何人。</p></br>" +
                 "<p>此邮件由系统自动发送，请勿回复此邮件。</p></br></br>" +
                 "<p>咸闲贤鱼</p></br>" +
-                new TimeUtils().getForMatTime());
+                TimeUtils.timeUtils.getForMatTime());
         MailUtils mail = new MailUtils(email,"验证码",text.toString());
         mail.sendMail();
     }
