bd25563fee90c9a5096e08963beee31a30bedf9c
limexc <zhiyuan1518>
2021-03-20 22:16:04 +0800

    完善功能

--- src/main/java/cn/limexc/controller/AccountController.java
+++ src/main/java/cn/limexc/controller/AccountController.java
@@ -5,16 +5,17 @@ import cn.limexc.service.UserService;
 import cn.limexc.util.MailUtils;
 import cn.limexc.util.StrMd5Utils;
 import cn.limexc.util.TimeUtils;
+import cn.limexc.util.VeCodeUtils;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.ModelAndView;
 
 import javax.annotation.Resource;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
-import java.util.Locale;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -47,25 +48,24 @@ public class AccountController {
         // 字符串是否与正则表达式相匹配
         boolean rs = matcher.matches();
 
-        System.out.println("邮箱："+email+" 密码："+password+" 邮箱格式："+rs);
-
-        //将查询到的数据放到user中
-        user = userService.login(email,password);
-        System.out.println("用户详细信息："+user.toString());
-        System.out.println(StrMd5Utils.MD5(user.getPassword()));
 
         if (rs && password!=null && password!=""){
+
+            System.out.println("邮箱："+email+" 密码："+password+" 邮箱格式："+rs);
+            //将查询到的数据放到user中
+            user = userService.login(email,password);
             if (user!=null){
+                System.out.println("用户详细信息："+user.toString());
+                System.out.println(StrMd5Utils.MD5(user.getPassword()));
                 //mv.setViewName("redirect:/index.jsp");
-                httpSession.setAttribute("id","");
+                //httpSession.setAttribute("id","");
                 return "forward:/index.jsp";
             }
+            return "forward:/login.jsp";
         }else {
             model.addAttribute("msg","登录失败，请检查用户名密码");
             return "forward:/login.jsp";
         }
-
-        return null;
 
     }
 
@@ -83,21 +83,22 @@ public class AccountController {
      * 明天再改改，不应该这样写
      * @param req
      */
-    @RequestMapping(value = "/sendmail")
+    @RequestMapping(value = "/sendmail",method = RequestMethod.POST)
     @ResponseBody
     public void sendMail(HttpServletRequest req){
-        String email = req.getParameter("email");
 
-        System.out.println("["+TimeUtils.timeUtils.getForMatTime()+"]"+"获取到用户输入的邮箱： "+email);
+        String email = req.getParameter("email");
 
         StringBuffer text=new StringBuffer("<h1>来自CloudWeb的验证码</h1>");
         text.append("<h3>用户："+email+" 您好</h3>");
-        text.append("本次的验证码为");
-        text.append("<p>如果您没有在注册账号或更改您的账号信息，请忽略此电子邮件。</p></br>");
+        text.append("本次的验证码为: <h4>"+ new VeCodeUtils().getVeCode(email) + "</h4>");
+        text.append("<p>如果您没有在<a href=\"#\">小贤网盘</a>注册账号或更改您的账号信息，请忽略此电子邮件。</p></br>" +
+                "<h5>验证码有效时间为10分钟</h5>");
         text.append("<p>不要把链接给任何人。</p></br>" +
                 "<p>此邮件由系统自动发送，请勿回复此邮件。</p></br></br>" +
-                "<p>咸闲贤鱼</p></br>" +
-                TimeUtils.timeUtils.getForMatTime());
+                "<p style=\"disable:block; left:60px;\">咸闲贤鱼</p></br>"+
+                "<p>"+TimeUtils.getUtils().getForMatTime()+"</p>"
+        );
         MailUtils mail = new MailUtils(email,"验证码",text.toString());
         mail.sendMail();
     }
